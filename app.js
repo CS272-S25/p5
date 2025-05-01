5d0094f25fd97f49516e842e8e81b01bd139e340
Fyang275 <fyang275@wisc.edu>
2025-05-01 15:20:24 -0500

    Update app.js
    Added the links so that My photos will show please add your links in also

--- app.js
+++ app.js
@@ -76,29 +76,79 @@ const products = {
 
   pensandpencils: [
     {
-      name: "Pilot G2 Premium Gel Roller Pens, Bold Point 1 mm, Pack of 2, Black",
-      image: "images/G2_premium_pens.jpg",
-      description: "Smooth & Long-lasting: Enjoy a smooth writing experience thanks to the gel ink that glides effortlessly across paper.",
-      price: "$2.98"
+      name: "Bic Mechanical Pencil #2 EXTRA SMOOTH, 40 Pack Of Twenty 0.5mm, and Twenty 0.7 mm - Assorted Colored Barrels - Variety Pack of Bic Lead Pencils",
+        image: "https://m.media-amazon.com/images/I/81LGb9fhIoL._AC_SX679_.jpg",
+        description: "Large Convenient value pack of 40 count click mechanical pencils.",
+        price: "$17.99"
     },
     {
-      name: "Sharpie S-Gel, Gel Pens, 12 Count, Black Ink",
-      image: "images/sharpie_premium_pens.jpg",
-      description: "Sharpie Quality: Reliable performance and best-in-class ink quality.",
-      price: "$12.65"
-    },
+    name: "Nicpro Gold Art Mechanical Pencil Set, Metal Drafting Pencil 0.5, 0.7, 0.9mm & 2mm Lead Holder(2B HB 2H) For Sketching Drawing With Lead Refills Case",
+        image: "https://m.media-amazon.com/images/I/819W64VtPuL._AC_SX679_.jpg",
+        description: "Value Package :Come With 6 PCS mechanical drafting pencils ",
+        price: "$16.99"
+  },
+  {
+    name: "Pentel Twist-Erase III Mechanical Pencil, (0.9mm), Assorted Barrel Colors with 2 Eraser Refills, Pack of 2",
+        image: "https://m.media-amazon.com/images/I/71abG3L-dGL._AC_SX679_.jpg",
+        description: "Sturdy, well-balanced barrel makes this the most popular Twist-Erase Pencil.",
+        price: "$10.09"
+  },
+  
     {
       name: "Paper Mate Clearpoint Mechanical Pencils, 0.7mm, Assorted Colors, 6 Count",
-      image: "images/papermate_clearpoint.jpg",
+      image: "https://m.media-amazon.com/images/I/81kMzn3SrEL.__AC_SX300_SY300_QL70_FMwebp_.jpg",
       description: "Mechanical pencils that create clear, precise lines every time.",
       price: "$12.99"
     },
     {
       name: "Ticonderoga Wood-Cased Pencils, #2 HB Soft, Yellow, 12 Count",
-      image: "images/ticonderoga_pencils.jpg",
+      image: "https://m.media-amazon.com/images/I/71ZBiCWIo-L._AC_SX679_.jpg",
       description: "Exclusive #2 graphite formula for extra smooth performance; thick graphite core delivers broad consistent lines.",
-      price: "$2.94"
+      price: "$4.50"
     },
+    {
+    name: "Write Dudes USA Gold Premium Cedar No. 2 Pre-Sharpened Pencils 12-Count",
+      image: "https://m.media-amazon.com/images/I/81g2YlJBmdL._AC_SY879_.jpg",
+      description: "Write Dudes USA Gold Premium Cedar No. 2 Pre-Sharpened Pencils 12-Count.",
+      price: "$2.79"
+  },
+  {
+    name: "Gel Pens, 5 Pcs 0.5mm Black Ink Pens Fine Point Smooth Writing Pens, High-End Series Pens for Journaling Note Taking",
+      image: "https://m.media-amazon.com/images/I/61pXbtOUuRL.__AC_SX300_SY300_QL70_FMwebp_.jpg",
+      description: "Package Included: These journal pens included 5 pcs of high-end morandi gel pens. They are great retractable pens for aesthetic school supplies, office supplies, journaling, note taking and planning.",
+      price: "$7.99"
+  },
+  {
+    name: "Scriveiner Black Lacquer Rollerball Pen - Stunning Luxury Pen with real gold plating Finish, Schmidt Ink Refill, Best Roller Ball Gift Set for Men & Women, Professional, Executive Office, Nice Pens",
+      image: "https://m.media-amazon.com/images/I/71XerdLrgrL._AC_SX679_.jpg",
+      description: "Enjoy the feel of a true luxury pen in your hand, Stunning luxury rollerball pen by Scriveiner. Brass with jet-black lacquer finished in real gold plating. Premium quality and understated luxury from a boutique British brand.",
+      price: "$32.99"
+  },
+  {
+    name: "Paper Mate Flair Felt Tip Pens, Medium Point (0.7mm), Assorted Colors, 12 Count",
+        image: "https://m.media-amazon.com/images/I/81X1SjEnOyL._AC_SX679_.jpg",
+        description: "Add a blast of fun to all your writing with bright colors.",
+        price: "$9.49"
+  },
+  {
+    name: "30 Colors Felt Tip Pens, 30 Pack, 0.7mm Premium Medium Fine Point",
+        image: "https://m.media-amazon.com/images/I/91eqZyHRlwL._AC_SX679_.jpg",
+        description: "VALUE PACK: 30 multi colors bulk pack Ezzgol felt tip pens set.",
+        price: "$13.99"
+  },
+  {
+    name: "Pilot G2 Premium Gel Roller Pens, Bold Point 1 mm, Pack of 2, Black",
+    image: "https://www.ndsubookstore.com/Product%20Images/07283831014_media-01.default.jpeg?resizeid=3&resizeh=600&resizew=600",
+    description: "Smooth & Long-lasting: Enjoy a smooth writing experience thanks to the gel ink that glides effortlessly across paper.",
+    price: "$2.98"
+  },
+  {
+    name: "Sharpie S-Gel, Gel Pens, 12 Count, Black Ink",
+    image: "https://m.media-amazon.com/images/I/81gFhr9VWLL._AC_SX679_.jpg",
+    description: "Sharpie Quality: Reliable performance and best-in-class ink quality.",
+    price: "$12.65"
+  },
+
   ],
 
   accessories: [
